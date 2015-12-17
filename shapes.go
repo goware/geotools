4eef4582b8fbf0cd44e92e1366ea3c113315ca5d
Fergal Walsh <fergalwalsh@gmail.com>
2015-12-17 15:14:26 +0200

    Added wkt encoding and wkb decoding to Point type for interfacing with PostGIS.

--- shapes.go
+++ shapes.go
@@ -30,6 +30,42 @@ func (p Point) LatLon() []float64 {
 	return []float64{p.Coordinates[1], p.Coordinates[0]}
 }
 
+func (p Point) MarshalDB() (interface{}, error) {
+	return p.WKT(), nil
+}
+
+func (p *Point) UnmarshalDB(v interface{}) error {
+	b, err := hex.DecodeString(v.(string))
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewReader(b)
+	type pointEWKB struct {
+		Endiness byte
+		Type     uint32
+		SRID     uint32
+		X        float64
+		Y        float64
+	}
+	var ewkb pointEWKB
+	err = binary.Read(buf, binary.LittleEndian, &ewkb)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return err
+	}
+	p.Type = "point"
+	p.Coordinates = []float64{ewkb.X, ewkb.Y}
+	return nil
+}
+
+func (p Point) WKT() string {
+	return fmt.Sprintf("POINT(%0.6f %0.6f)", p.Coordinates[0], p.Coordinates[1])
+}
+
+func (p Point) String() string {
+	return p.WKT()
+}
+
 // Envelope is a GeoJSON like shape where coordinates contains [[left, top], [right, bottom]]
 type Envelope struct {
 	Type        string      `json:"type"`
