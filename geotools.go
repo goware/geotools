7a2ba5f420a3393798d7f4ca57b2a981582f4e14
Peter Kieltyka <peter.kieltyka@pressly.com>
2016-03-15 16:09:16 -0400

    Readme update

--- geotools.go
+++ geotools.go
@@ -1,7 +1,7 @@
-package geoy
+package geotools
 
 import (
-	"github.com/pressly/geoy/gmaps"
+	"github.com/pressly/geotools/gmaps"
 	"golang.org/x/net/context"
 )
 
--- geotools_test.go
+++ geotools_test.go
@@ -1,4 +1,4 @@
-package geoy
+package geotools
 
 import (
 	"encoding/json"
